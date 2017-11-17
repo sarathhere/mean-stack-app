cc52227743ecfd7dd316217b0c4ababe10b8205f
ATTRAGROUP\sarath.p <sarathhere@gmail.com>
2017-11-17 18:18:27 +0530

    day two commit

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname,'public')));
+/*app.get('/',function(req, res){
+	console.log("Get the home page");
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+*/
+app.get('/json',function(req, res){
+	console.log("Get the JSON object");
+	res
+		.status(200)
+		.json({"jsonData" : true});
+});
+
+app.get('/file',function(req, res){
+	console.log("Get the FILE object");
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Express JS listening on Port : ' + port);
+});
+
+
+
+
+
+/*
+//Lesson 1
 require('./instantHello');
 var goodbye = require('./talk/goodbye');
 var talk = require('./talk');
@@ -7,4 +44,4 @@ talk.intro();
 talk.hello('Sarath');
 var answer = question.ask('Am I learning something ?');
 console.log(answer);
-goodbye();
+goodbye();*/
